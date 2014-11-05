e79259a55e7b4ac9fbef232427e78c96317c2fb1
pewtroof <mandre.savard@gmail.com>
2014-11-05 17:05:37 -0500

    Client interface form modeling

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,8 @@
 /**
  * Created by coylter on 11/4/2014.
  */
-public class Client {
+public class Client
+{
     public static void main(String [] args){
         System.out.println("Amazing");
     }
