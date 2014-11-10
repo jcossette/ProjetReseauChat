05c8ede44e1720962bb09a2d309a276ae742a4cd
Julien Cossette <juliencossette22@gmail.com>
2014-11-10 15:56:25 -0500

    MAJUSCULE MOTHERFUCKER DO YOU SPEAK IT

---
+++ src/Server/ChatManagerJob.java
@@ -0,0 +1,8 @@
+package Server;
+
+/**
+ * This job runs a facade managing the different components of the chat server.
+ * Created by Julien Cossette on 11/10/2014.
+ */
+public class ChatManagerJob {
+}
