01f0e226f4a539f158fa58c304b5a103d859e01a
pewtroof <mandre.savard@gmail.com>
2014-11-10 15:33:47 -0500

    Merge branch 'master' of https://github.com/jcossette/ProjetReseauChat into develop
    Conflicts:
    	src/Client.java

---
+++ src/ClientGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by pewtroof on 2014-11-05.
+ */
+public class ClientGUI {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JButton button1;
+    private JList list1;
+    private JTextArea textArea1;
+}
