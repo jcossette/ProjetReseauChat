df72b9305f43d041212301a0a5d5ca3d41c70dbf
Carl Fillion <https://cfillion@bitbucket.org>
2014-12-08 16:34:06 -0500

    ajout de commentaire

--- src/GUI/ClientGUI.java
+++ src/GUI/ClientGUI.java
@@ -42,7 +42,7 @@ public class ClientGUI extends JFrame
 
         clientController = ClientController.getInstance();
 
-        setTitle("Form 1.1");
+        setTitle("Super chat");
         setContentPane(entryPanel);
         setLocationRelativeTo(null);
         pack();
@@ -52,6 +52,7 @@ public class ClientGUI extends JFrame
         roomList = new ArrayList();
         roomMap = new HashMap();
 
+        /** Envoi le texte lorsqu'on appuie sur le bouton Send */
         buttonSend.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
@@ -61,6 +62,7 @@ public class ClientGUI extends JFrame
             }
         });
 
+        /** Envoi le texte lorsqu'on appuie sur Enter */
         textFieldInputText.addKeyListener(
                 new KeyAdapter()
                 {
@@ -74,6 +76,7 @@ public class ClientGUI extends JFrame
                 }
         );
 
+        /** Met a jour le texte et la liste d'utilisateur lorsqu'on change de tab/Room */
         tabbedPaneRoom.addChangeListener(new ChangeListener()
         {
             public void stateChanged(ChangeEvent e)
@@ -84,12 +87,16 @@ public class ClientGUI extends JFrame
                 fillTab(room);
             }
         });
+
+        /** Envoi un colis getRoomList au serveur lorsqu'on appuie sur le boutton Add Room */
         buttonAddRoom.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 clientController.getRoomList();
             }
         });
+
+        /** Envoi un colis leaveRoom au serveur lorsqu'on appuie sur le boutton Leave Room et delete la tab courante*/
         buttonLeaveRoom.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -100,7 +107,9 @@ public class ClientGUI extends JFrame
         });
     }
 
-    //Envoi un paquet communication lors"on appuie sur "Enter" ou sur le bouton "Send"
+    /**
+     * Envoi un paquet communication lors"on appuie sur "Enter" ou sur le bouton "Send"
+     **/
     private void sendCommunication(){
         if(!textFieldInputText.getText().isEmpty())
         {
@@ -109,13 +118,17 @@ public class ClientGUI extends JFrame
         }
     }
 
-    //Retourne la room courante
+    /**
+     * Retourne la room courante
+     **/
     public Room getCurrentRoom(){
         Room currentRoom = roomList.get(tabbedPaneRoom.getSelectedIndex());
         return currentRoom;
     }
 
-    //Update le JTextArea output lorsqu'on recoit un colis updateText du serveur
+    /**
+     * Update le JTextArea output lorsqu'on recoit un colis updateText du serveur
+     **/
     public void updateText(Integer roomID, String text)
     {
         Room toUpdate = getRoom(roomID);
@@ -127,14 +140,18 @@ public class ClientGUI extends JFrame
         toUpdateField.repaint();
     }
 
-    //Ajoute un nom a la liste d'utilisateur
+    /**
+     * Ajoute un nom a la liste d'utilisateur
+     **/
     private void updateAddName(String name)
     {
         model.addElement(name);
         listName.setModel(model);
     }
 
-    //Enleve un nom de la liste d'utilisateur
+    /**
+     * Enleve un nom de la liste d'utilisateur
+     **/
     public  void updateRemoveName(String userName)
     {
         for(int i = 0; i<model.getSize(); i++)
@@ -148,7 +165,9 @@ public class ClientGUI extends JFrame
         listName.setModel(model);
     }
 
-    //Ajoute un utilisateur a une room particuliere
+    /**
+     * Ajoute un utilisateur a une room particuliere
+     **/
     public void addNameToRoom(String roomName, User user)
     {
         Room room = getRoom(roomName);
@@ -158,14 +177,16 @@ public class ClientGUI extends JFrame
         room.addUser(user);
     }
 
-    //Enleve un utilisateur d'une room particuliere
+    /**
+     * Enleve un utilisateur d'une room particuliere
+     **/
     public void removeNameFromRoom(Room room, User userToRemove)
     {
         if(isCurrentRoom(room)) {
             updateRemoveName(userToRemove.getUsername());
         }
 
-        //Necessaire a cause que les references d'user du server et du client ne sont pas les memes
+        /** Necessaire a cause que les references d'user du server et du client ne sont pas les memes  **/
         for (Iterator<User> userIterator = room.getMyUsers().iterator(); userIterator.hasNext(); ) {
             User user = userIterator.next();
             if (user.getUsername().equals(userToRemove.getUsername())) {
@@ -174,19 +195,26 @@ public class ClientGUI extends JFrame
         }
     }
 
+    /**
+     * Enleve un utilisateur d'une Room 'a partir de l'ID de la Room
+     **/
     public void removeNameFromRoom(int roomID, User userToRemove){
         Room roomToRemove = getRoom(roomID);
         removeNameFromRoom(roomToRemove, userToRemove);
     }
 
-    //Enleve un utilisateur de toutes les rooms
+    /**
+     * Enleve un utilisateur de toutes les rooms
+     **/
     public void removeNameFromAllRooms(User user) {
         for (Room room : roomList){
             removeNameFromRoom(room, user);
         }
     }
 
-    //Recoi les informations du lobby du serveur
+    /**
+     * Recoit les informations du lobby du serveur
+     **/
     public void fullUpdate(List<Room> roomList)
     {
         Room lobby = roomList.get(0);
@@ -196,7 +224,9 @@ public class ClientGUI extends JFrame
         fillTab(lobby);
     }
 
-    //Va chercher la room qu'on vient de selectionner et affiche le texte et la liste d'utilisateurs
+    /**
+     * Va chercher la room qu'on vient de selectionner et affiche le texte et la liste d'utilisateurs
+     **/
     private void fillTab(Room room){
         for(User user : room.getMyUsers()) {
             updateNameFromList(user.getUsername());
@@ -208,7 +238,9 @@ public class ClientGUI extends JFrame
         }
     }
 
-    //Cree une tab pour une nouvelle room
+    /**
+     * Cree une tab pour une nouvelle room
+     **/
     public void createRoomTab(Room room){
         JPanel panel = new JPanel(new BorderLayout());
         JTextArea textArea = new JTextArea();
@@ -229,7 +261,9 @@ public class ClientGUI extends JFrame
         textAreaOutputText.append(text + "\n");
     }
 
-    //Va chercher l'objet Room a partir de son nom
+    /**
+     * Va chercher l'objet Room a partir de son nom
+     **/
     private Room getRoom(String roomName){
         Room room = new Room();
 
@@ -243,7 +277,9 @@ public class ClientGUI extends JFrame
         return room;
     }
 
-    //Va chercher l'objet Room a partir de son ID
+    /**
+     * Va chercher l'objet Room a partir de son ID
+     **/
     private Room getRoom(Integer ID){
         Room room = null;
         for (Room r : roomList){
@@ -255,7 +291,9 @@ public class ClientGUI extends JFrame
         return room;
     }
 
-    //Verifie si la Room passe en parametre est la Room courante
+    /**
+     * Verifie si la Room passe en parametre est la Room courante
+     **/
     private boolean isCurrentRoom(Room room){
         if (room.getName().equals(getCurrentRoom().getName())) {
             return true;
